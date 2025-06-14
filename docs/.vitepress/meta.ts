09017d3661df06631d54cd5f41bde3c59d93fc04
Johann Schopplich <mail@johannschopplich.com>
2025-06-14 09:58:09 +0200

    chore: update social card images

--- docs/.vitepress/meta.ts
+++ docs/.vitepress/meta.ts
@@ -2,5 +2,5 @@ export const name = "RTYI Documentary";
 export const description =
   "Beyond the cartridge: My personal space for researching and preparing the Return to Yoshi's Island documentary.";
 export const ogUrl = "https://rtyi.land/";
-export const ogImage = `${ogUrl}og.png`;
-export const twitterImage = `${ogUrl}twitter.png`;
+export const ogImage = `${ogUrl}og.jpg`;
+export const twitterImage = `${ogUrl}twitter.jpg`;
